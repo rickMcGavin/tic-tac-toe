88a52c1c07302d2010b83d7dd934c22193cce657
rebelBeard <rfm144@gmail.com>
2017-01-28 22:34:17 -0500

    more progress on game with JS

--- js/tic-tac-toe.js
+++ js/tic-tac-toe.js
@@ -1,8 +1,9 @@
+var count = 0;
+var random = 0;
 var playerX = "X";
 var playerO = "O";
 var player = playerX;
-var count = 0;
-var board = [];
+
 
 // ask user if one or two player (for now two player will be only option)
 function launchGameTypeModal() {
@@ -26,49 +27,103 @@ function launchGameTypeModal() {
 // ask user if they want to be x or o
 function launchGamePieceModal() {
   $("#choose-piece").show();
-  $("button").click(function() {
+  $("#button-x").click(function() {
     $("#choose-piece").hide();
+    launchXPlayerMode();
+  });
+  $("#button-o").click(function() {
+    $("#choose-piece").hide();
+    launchOPlayerMode();
   });
 }
 
-function init() {
-  launchGameTypeModal();
+function launchXPlayerMode() {
+  (function getRandomNumber() {
+    random = Math.floor(Math.random() * 9);
+    if ($("#"+random).text() === "X" || $("#"+random).text() === "O") {
+      getRandomNumber();
+    } else {
+      return random;
+    }
+  })();
+  if (count % 2 === 0) {
+    $(".square").unbind("click").click(function(element) {
+      takeTurn($(this));
+      $(this).off("click");
+      launchXPlayerMode();
+    });
+  } else {
+    takeTurn($("#"+random));
+    $("#"+random).off("click");
+    launchXPlayerMode();
+  }
 }
+
+function launchOPlayerMode() {
+  (function getRandomNumber() {
+    random = Math.floor(Math.random() * 9);
+    if ($("#"+random).text() === "X" || $("#"+random).text() === "O") {
+      getRandomNumber();
+    } else {
+      return random;
+    }
+  })();
+  if (count % 2 !== 0) {
+    $(".square").unbind("click").click(function(element) {
+      takeTurn($(this));
+      $(this).off("click");
+      launchOPlayerMode();
+    });
+  } else {
+    takeTurn($("#"+random));
+    $("#"+random).off("click");
+    launchOPlayerMode();
+  }
+}
+
 
 function reset() {
   $(".square").empty();
   $("#message").empty();
   $("#message").hide();
-  board = [];
-  count = 0;
   player = playerX;
+  count = 0;
   $(".square").removeClass("x o");
-  $(".square").unbind("click").click(function(element) {
-    updateBoard(element.target.id);
-    takeTurn($(this));
-    $(this).off("click");
-  });
-}
-
-function switchPlayer() {
-  count++;
-
-  if (player === playerX) {
-    player = playerO;
-  } else {
-    player = playerX;
-  }
 }
 
 function checkForWinner() {
   if (
-  (board[0] === player && board[1] === player && board[2] === player)     || (board[3] === player && board[4] === player && board[5] === player)     ||  (board[6] === player && board[7] === player && board[8] === player)     ||
-  (board[0] === player && board[3] === player && board[6] === player)     ||
-  (board[1] === player && board[4] === player && board[7] === player)     ||
-  (board[2] === player && board[5] === player && board[8] === player)     ||
-  (board[0] === player && board[4] === player && board[8] === player)     ||
-  (board[2] === player && board[4] === player && board[6] === player)
-  ) {
+   ($("#0").text() === player) &&
+   ($("#1").text() === player) &&
+   ($("#2").text() === player) ||
+
+   ($("#3").text() === player) &&
+   ($("#4").text() === player) &&
+   ($("#5").text() === player) ||
+
+   ($("#6").text() === player) &&
+   ($("#7").text() === player) &&
+   ($("#8").text() === player) ||
+
+   ($("#0").text() === player) &&
+   ($("#3").text() === player) &&
+   ($("#6").text() === player) ||
+
+   ($("#1").text() === player) &&
+   ($("#4").text() === player) &&
+   ($("#7").text() === player) ||
+
+   ($("#2").text() === player) &&
+   ($("#5").text() === player) &&
+   ($("#8").text() === player) ||
+
+   ($("#0").text() === player) &&
+   ($("#4").text() === player) &&
+   ($("#8").text() === player) ||
+
+   ($("#2").text() === player) &&
+   ($("#4").text() === player) &&
+   ($("#6").text() === player)) {
       $("#message").append("<h1>" + player + " wins!</h1>");
       $("#message").show();
       setTimeout(function() {
@@ -83,25 +138,28 @@ function checkForWinner() {
   }
 }
 
-function updateBoard(squareId) {
-  board[squareId] = player;
-  console.log(board[7]);
+function launchTwoPlayerMode() {
+  $(".square").unbind("click").click(function(element) {
+    takeTurn($(this));
+    console.log($(this).text());
+    $(this).off("click");
+  });
+}
+
+function switchPlayer() {
+  count++;
+  if (player === playerX) {
+    player = playerO;
+  } else {
+    player = playerX;
+  }
 }
 
 function takeTurn(cell) {
-  cell.addClass(player.toLocaleLowerCase());
+  cell.addClass(player.toLowerCase());
   cell.text(player);
   checkForWinner();
   switchPlayer();
 }
 
-function launchTwoPlayerMode() {
-  $(".square").unbind("click").click(function(element) {
-    updateBoard(element.target.id);
-    takeTurn($(this));
-    $(this).off("click");
-  });
-}
-
-
-init();
+launchGameTypeModal();
