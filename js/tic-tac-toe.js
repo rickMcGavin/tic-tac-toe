5c4a205b534c34be0742a77c5ad72411f2f3d9db
rebelBeard <rfm144@gmail.com>
2017-01-28 22:22:00 -0500

    updates to js

--- js/tic-tac-toe.js
+++ js/tic-tac-toe.js
@@ -7,12 +7,20 @@ var board = [];
 // ask user if one or two player (for now two player will be only option)
 function launchGameTypeModal() {
   $("#onePlayer-twoPlayer").show();
-  $("#two-player").click(function() {
+  $("#two-player").unbind("click").click(function() {
     $("#onePlayer-twoPlayer").hide();
-    alert("X always goes first");
+    $("#message").show();
+    $("#message").append("<h1>X always goes first</h1>");
+    setTimeout(function() {
+      $("#message").empty();
+      $("#message").hide();
+    }, 1000);
     launchTwoPlayerMode();
   });
-
+  $("#one-player").click(function() {
+    $("#onePlayer-twoPlayer").hide();
+    launchGamePieceModal();
+  });
 }
 
 // ask user if they want to be x or o
@@ -25,7 +33,21 @@ function launchGamePieceModal() {
 
 function init() {
   launchGameTypeModal();
+}
 
+function reset() {
+  $(".square").empty();
+  $("#message").empty();
+  $("#message").hide();
+  board = [];
+  count = 0;
+  player = playerX;
+  $(".square").removeClass("x o");
+  $(".square").unbind("click").click(function(element) {
+    updateBoard(element.target.id);
+    takeTurn($(this));
+    $(this).off("click");
+  });
 }
 
 function switchPlayer() {
@@ -47,19 +69,23 @@ function checkForWinner() {
   (board[0] === player && board[4] === player && board[8] === player)     ||
   (board[2] === player && board[4] === player && board[6] === player)
   ) {
-    $("#winner").append("<h1>" + player + " wins!</h1>");
-    $("#winner").show();
-  }
-
-  if (board)
-  if (count >= 8) {
-    alert("It's a draw");
+      $("#message").append("<h1>" + player + " wins!</h1>");
+      $("#message").show();
+      setTimeout(function() {
+        reset();
+      }, 2000);
+  } else if (count >= 8) {
+      $("#message").append("<h1>It's a draw</h1>");
+      $("#message").show();
+      setTimeout(function() {
+        reset();
+      }, 2000);
   }
 }
 
 function updateBoard(squareId) {
   board[squareId] = player;
-  console.log(board);
+  console.log(board[7]);
 }
 
 function takeTurn(cell) {
